2f6796ceebf85c91825f5b561a5cead8c50af99e
turkmenay <a.turkmenay@gmail.com>
2021-12-15 23:31:31 +0300

    ilk proje

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Adınızı Girin :");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı Girin :");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba " + name + " " + surname);
+
+            
+        }
+    }
+}
